14fd4cc46b9dbcfe6a1dc9940328f7ecaabad9e0
KKD98 <KajolKona98@gmail.com>
2023-05-18 15:00:24 +0600

    banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import banner1 from '../../assets/banner1.jpg'
+import banner2 from '../../assets/banner2.jpg'
+import banner3 from '../../assets/banner3.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <p>I am banner</p>
+        <div className="carousel w-full mb-4  bg-gradient-to-r from-cyan-200 to-blue-400">
+            <div id="slide1" className="carousel-item relative w-full">
+
+                <div className="hero min-h-screen ">
+                    <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+                        <img src={banner1} className="w-full lg:w-[50%]  lg:p-5" />
+                        <div className='pl-8'>
+                            <h1 className="text-5xl font-bold text-blue-700">Doll</h1>
+                            <p className="py-6 text-blue-700 font-semibold">Explore limitless opportunities with these dolls that open up a world <br /> of endless possibilities.</p>
+                            <button className="btn bg-white text-blue-700 border-none hover:bg-blue-400 hover:text-white">Show More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+            <div className="hero min-h-screen ">
+                    <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+                        <img src={banner2} className="w-full lg:w-[50%]  lg:p-5" />
+                        <div className='pl-8'>
+                            <h1 className="text-5xl font-bold text-blue-700">Lego</h1>
+                            <p className="py-6 text-blue-700 font-semibold">Spark creativity and endless fun with building playsets and more to inspire imaginations.</p>
+                            <button className="btn bg-white text-blue-700 border-none hover:bg-blue-400 hover:text-white">Show More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❮</a>
+                    <a href="#slide3" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+            <div className="hero min-h-screen ">
+                    <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+                        <img src={banner3} className="w-full lg:w-[50%]  lg:p-5" />
+                        <div className='pl-8'>
+                            <h1 className="text-5xl font-bold text-blue-700">Action Figure</h1>
+                            <p className="py-6 text-blue-700 font-semibold">Prepare for an exciting experience with these thrilling and captivating choices that spark your sense of adventure.</p>
+                            <button className="btn bg-white text-blue-700 border-none hover:bg-blue-400 hover:text-white">Show More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❮</a>
+                    <a href="#slide1" className="btn btn-circle bg-white border-none text-blue-700 text-2xl hover:bg-purple-400">❯</a>
+                </div>
+            </div>
         </div>
     );
 };
