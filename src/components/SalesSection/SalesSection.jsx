c0734371e3d69b206dbbab19eca51a37602788ac
KKD98 <KajolKona98@gmail.com>
2023-05-20 15:25:30 +0600

    used aos in sales section

--- src/components/SalesSection/SalesSection.jsx
+++ src/components/SalesSection/SalesSection.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import pic from '../../assets/salespic.jpg'
+import React, { useEffect } from 'react';
+import pic from '../../assets/salespic.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 
 const SalesSection = () => {
+    useEffect(() => {
+        AOS.init();
+    } , [])
     return (
-        <div className='relative my-8 w-full'>
+        <div data-aos="flip-left"  data-aos-delay="500" data-aos-duration="600" className='relative my-8 w-full'>
             <img className='mx-auto w-full lg:w-[70%] ' src={pic} alt="" />
             <div className='absolute h-full w-full lg:w-[70%] left-0 lg:left-48  top-0 bg-gradient-to-r from-[#151520] to-[rgba(21 , 21 , 21 , 0)] text-white'>
                <div className='mx-auto w-full ml-10 lg:ml-48 mt-24 lg:mt-48'>
